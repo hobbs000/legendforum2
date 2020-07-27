1775e40df2480631560d305c6009193dfeab0471
hobbs000 <64557379+hobbs000@users.noreply.github.com>
2020-07-27 22:47:47 +0300

    Add files via upload

---
+++ ana-dizin/tum-uyeler.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+
+
+	require_once( dirname(__FILE__) . '/wp-load.php' );
+require_once( dirname(__FILE__) . '/wp-blog-header.php' );
+
+
+	require_once( ABSPATH . '/wp-content/themes/forum-Officespace/tum-uyeler.php' );
+
+
+ 
+?>
